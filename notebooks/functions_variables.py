0603407da93077cc601c4e5b63f1c88d87924de5
FranklinManuana <65874398+FranklinManuana@users.noreply.github.com>
2025-05-20 15:37:10 -0600

    Added following functions: JSON_extractrion, normalized_dataframe, drop_columns. Added variable: results

--- notebooks/functions_variables.py
+++ notebooks/functions_variables.py
@@ -1,3 +1,11 @@
+#import necessary libraries
+import pandas as pd
+import numpy as np
+import os
+import json
+
+
+
 def encode_tags(df):
 
     """Use this function to manually encode tags from each sale.
@@ -14,3 +22,34 @@ def encode_tags(df):
     # create a unique list of tags and then create a new column for each tag
         
     return df
+
+# Extract house data from all JSON files in data folder
+def json_extraction(direc='../data'):
+    results = []
+    count = 0
+    for file_name in os.listdir(direc):
+        if file_name.endswith(".json"):
+            json_path = os.path.join(direc, file_name)
+            with open(json_path) as f:
+                file_data = json.load(f)
+            data = file_data['data']['results']
+            results.append(data)
+            count+=1
+    print(f'House information extracted from the {count} JSON files in data folder')
+    return results
+
+results = json_extraction()
+
+def normalized_dataframe(results):
+    # create empty data_set
+    df_set = []
+    # loop cycles through each result individual normalization
+    for i in range(len(results)):
+        df = pd.json_normalize(results[i])
+        df_set.append(df)
+    df = pd.concat(df_set)
+    return df
+
+def drop_columns(dataframe = {},drop_attributes = []):
+    refined_df = dataframe.drop(columns= drop_attributes)
+    return refined_df
